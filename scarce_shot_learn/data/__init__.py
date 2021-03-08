cccdfbf3406cf1a11c8dadf64d0a74b969ba5dbc
lambdaofgod <bartczukkuba@gmail.com>
2021-03-08 21:08:36 +0100

    better submodule naming

---
+++ scarce_shot_learn/data/__init__.py
@@ -0,0 +1,10 @@
+from .cub import load_cub
+
+
+def load_awa2():
+    """
+    Animals with Attributes 2 Dataset
+    returns dict split: (X_split, attribute_features_split, labels_split)
+    """
+    awa_path = os.path.join('data', 'xlsa17', 'data', 'AWA2')
+    return load_dataset(awa_path)
--- test/test_zero_shot.py
+++ test/test_zero_shot.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scarce_shot_learn.zero import ESZSLearner
+from scarce_shot_learn import zero_shot
 
 
 def test_attribute_to_labels():
@@ -7,7 +7,7 @@ def test_attribute_to_labels():
     y = np.array(['even', 'odd', 'even', 'odd'])
     attributes = np.eye(2)
 
-    eszslearner = ESZSLearner()
+    eszslearner = zero_shot.ESZSLearner()
     eszslearner.fit(X, y, attributes)
 
     prediction = eszslearner.predict(X, attributes, np.array(['even', 'odd']))
